85146bcc54ef130c44eb262d75984dabd1240d86
gairik <gairikgmandal@gmail.com>
2025-03-01 11:45:56 +0530

    userwithlinks controller is done

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
 const User = require("../models/userModel");
+const Link = require("../models/linkModel");
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
@@ -67,6 +69,38 @@ const updateUser = async (req, res) => {
   }
 };
 
+const getUserWithLinks = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    // Validate MongoDB ID
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: "Invalid user ID format" });
+    }
+
+    // Get user without sensitive fields
+    const user = await User.findById(userId).select("-password -agree").lean();
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Get associated links and exclude user reference
+    const links = await Link.find({ user: userId }).select("-user");
+
+    // Add links to user object
+    user.links = links;
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching user data",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   updateUser,
+  getUserWithLinks,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const validateToken = require("../middleware/authMiddleware");
-const { updateUser } = require("../controller/userController");
+const {
+  updateUser,
+  getUserWithLinks,
+} = require("../controller/userController");
 const router = express.Router();
 
 router.route("/updateuser").patch(validateToken, updateUser);
+router.route("/userwithlinks/:userId").get(getUserWithLinks);
 
 module.exports = router;
